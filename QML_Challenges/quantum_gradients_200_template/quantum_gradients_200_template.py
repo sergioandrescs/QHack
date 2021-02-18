99173f664853406054c05efcd531ed32f277e684
Sergio Cotrino <sergio.cotrino@gobrilliant.com>
2021-02-18 17:17:16 -0500

    Quantum Gradients 200 resolved. Piuffff

--- QML_Challenges/quantum_gradients_200_template/quantum_gradients_200_template.py
+++ QML_Challenges/quantum_gradients_200_template/quantum_gradients_200_template.py
@@ -47,6 +47,70 @@ def gradient_200(weights, dev):
 
     # QHACK #
 
+    # Gradient Calculation
+    import math
+
+    in_place_calculation = circuit(weights)
+    half_pi_shifts = []
+
+    def parameter_shift_term(qnode, params, shift_value, index):
+        shifted = params.copy()
+        shifted[index] += shift_value
+        forward = qnode(shifted)  # forward evaluation
+        half_pi_shifts.append(forward)
+
+        shifted[index] -= shift_value*2
+        backward = qnode(shifted)  # backward evaluation
+        half_pi_shifts.append(backward)
+
+        return (forward - backward)/(2*math.sin(shift_value))
+
+    def shifter(params, shift_value, row, col, signs):
+        r""" This function executes the shift needed for the Hessian calculation.
+
+        Args:
+            row (int): First index of the shift
+            col (int): Second index of the shift
+            signs (tuple): Tuple with signs (+1,-1) os the shift to be made
+
+        Returns: 
+            array: Shifted params 
+        """
+        shifted = params.copy()
+        shifted[row] += shift_value*signs[0]
+        shifted[col] += shift_value*signs[1]
+
+        return shifted
+
+    def parameter_hessian_term(qnode, params, shift_value, row, col):
+        # Reference for future review: https://arxiv.org/pdf/2008.06517.pdf
+        # To optimize, pi/2 and pi/4 as shift values were considered.
+        # DO NOT FORGET
+        if row == col and shift_value == math.pi/2:
+            shifted = params.copy()
+            shifted[row] += math.pi
+
+            forward = half_pi_shifts[row*2]
+            backward = half_pi_shifts[row*2 + 1]
+
+            return (forward - (2*in_place_calculation) + backward)/2
+        else:
+            step_1 = qnode(shifter(params, shift_value, row, col, [1, 1]))
+            step_2 = qnode(shifter(params, shift_value, row, col, [-1, 1]))
+            step_3 = qnode(shifter(params, shift_value, row, col, [1, -1]))
+            step_4 = qnode(shifter(params, shift_value, row, col, [-1, -1]))
+
+            return (step_1 - step_2 - step_3 + step_4)/pow(2*math.sin(shift_value), 2)
+
+    for i in range(len(weights)):
+        gradient[i] = parameter_shift_term(circuit, weights, math.pi/2, i)
+
+    for i in range(len(weights)):
+        for j in range(i, len(weights)):
+            hessian[i][j] = parameter_hessian_term(
+                circuit, weights, math.pi/2, i, j)
+            hessian[j][i] = hessian[i][j]
+
     # QHACK #
 
     return gradient, hessian, circuit.diff_options["method"]
