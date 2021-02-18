70a3d9987846b01047c49f5eccf46b6fbbb1c704
Sergio Cotrino <sergio.cotrino@gobrilliant.com>
2021-02-18 01:12:07 -0500

    Work on circuit trainint QAOA 200.

--- QML_Challenges/circuit_training_200_template/circuit_training_200_template.py
+++ QML_Challenges/circuit_training_200_template/circuit_training_200_template.py
@@ -31,6 +31,40 @@ def find_max_independent_set(graph, params):
     max_ind_set = []
 
     # QHACK #
+    from pennylane import qaoa
+
+    cost_h, mixer_h = qaoa.max_independent_set(graph, constrained=False)
+
+    # A single layer of QAOA
+    def qaoa_layer(gamma, alpha):
+        qaoa.cost_layer(gamma, cost_h)
+        qaoa.mixer_layer(alpha, mixer_h)
+
+    # Now, create the layered circuit
+    depth = 10
+    wires = 6
+
+    def circuit(params, **kwargs):
+        qml.layer(qaoa_layer, depth, params[0], params[1])
+
+    # Device and eval of cost function
+    dev = qml.device("default.qubit", wires=wires)
+
+    @qml.qnode(dev)
+    def probability_circuit(gamma, alpha):
+        circuit([gamma, alpha])
+        return qml.probs(wires=[i for i in range(wires)])
+
+    probs = probability_circuit(params[0], params[1])
+    print(probs)
+
+    max_index = np.argmax(probs)
+
+    bin_max = "{0:b}".format(max_index)
+
+    for count, val in enumerate(bin_max):
+        if val == '1':
+            max_ind_set.append(count)
 
     # QHACK #
 
